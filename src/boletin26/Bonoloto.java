8ef12f2feb2c63f6dd145770f08b263f009a20e7
aferreiradominguez <aferreiradominguez@atanasoft05.danielcastelao.org>
2015-04-16 11:05:35 +0200

    haciendo random

--- src/boletin26/Bonoloto.java
+++ src/boletin26/Bonoloto.java
@@ -1,6 +1,7 @@
 package boletin26;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -11,7 +12,8 @@ public class Bonoloto implements ActionListener {
 
     private final JFrame primitiva;
     private JButton[][] botonMatriz;
-    private int filas = 10, columnas = 5;
+    private final int filas = 10, columnas = 5, maxAp = 6;
+    private int contador = 0;
 
     public Bonoloto() {
         primitiva = new JFrame();
@@ -20,6 +22,7 @@ public class Bonoloto implements ActionListener {
         primitiva.setSize(800, 600);
         primitiva.setVisible(true);
         primitiva.getContentPane().add(crearApuesta(), BorderLayout.CENTER);
+        primitiva.getContentPane().add(botones(), BorderLayout.SOUTH);
         primitiva.pack();
         primitiva.setMinimumSize(new Dimension(800, 600));
         primitiva.setLocationRelativeTo(null);
@@ -32,23 +35,58 @@ public class Bonoloto implements ActionListener {
         panel.setLayout(new GridLayout(filas, columnas, 2, 2));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         for (int i = 0; i < filas; i++) {
-            ac=i;
+            ac = i;
             for (int j = 0; j < columnas; j++) {
                 botonMatriz[i][j] = new JButton();
                 botonMatriz[i][j].addActionListener(this);
                 panel.add(botonMatriz[i][j]);
                 panel.setVisible(true);
                 botonMatriz[0][0].setEnabled(false);
+                botonMatriz[0][0].setText("apuesta");
                 botonMatriz[i][j].setText(String.valueOf(ac));
-                ac=ac+filas;
+                ac = ac + filas;
             }
         }
         return panel;
     }
 
+    private JPanel botones() {
+        JPanel panel = new JPanel();
+        JButton limpiar = new JButton("Limpiar");
+        JButton comprobar = new JButton("Comprobar boleto");
+        JTextField nWin = new JTextField(18);
+        JTextField nAcertado = new JTextField(10);
+        JLabel numPrem = new JLabel("numero premiado: ");
+        JLabel numAc = new JLabel("numero de aciertos: ");
+        limpiar.addActionListener(this);
+        comprobar.addActionListener(this);
+        panel.add(comprobar);
+        panel.add(numPrem);
+        panel.add(nWin);
+        panel.add(numAc);
+        panel.add(nAcertado);
+        panel.add(limpiar);
+        return panel;
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JButton eBoton = (JButton) e.getSource();
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (eBoton.equals(botonMatriz[i][j])) {
+                    botonMatriz[i][j].setBackground(Color.red);
+                    contador++;
+                    if (contador >= maxAp) {
+                        for (int k = 0; k < filas; k++) {
+                            for (int l = 0; l < columnas; l++) {
+                                botonMatriz[k][l].setEnabled(false);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
